10872760b3c23cffadd6d9793ac0150ab4be483d
THESHADOWKNIFE <yaoy@wsc.school.nz>
2024-05-06 11:58:32 +1200

    hbub

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-input ("What's your name?")
-print ("Welcome to this quiz.")
-print ("This quiz is about capital cities of the world.")
-input ("What's the capital city of New Zealand?")
-print ("The capital city of New Zealand is Wellington")
+# Get user's name
+name = input("What's your name?")
+# Greet the user and introduce the quiz
+if name == "Lorenzo":
+    print("That's cool.")
+    print("My name is Vegeta.")
+print("Let's play a game.")
+print("Welcome to the quiz,", name)
+print("This quiz is about capital cities of the world.")
+# Ask the user a question
+answer = input("What's the capital city of New Zealand?")
+if answer == "Wellington":
+    print("Correct!")
+print("The capital city of New Zealand is Wellington.")
