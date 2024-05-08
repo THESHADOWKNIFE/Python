c5bb8c34328bd0dcb375cb7695941e1a287c32eb
THESHADOWKNIFE <yaoy@wsc.school.nz>
2024-05-09 10:29:37 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+score = 0
 # Get user's name
 name = input("What's your name?")
 # Greet the user and introduce the quiz
@@ -9,12 +9,15 @@ print("Let's play a game.")
 print("Welcome to the quiz,", name)
 print("This quiz is about capital cities of the world.")
 # Ask the user a question
-answer = input("What's the capital city of New Zealand?")
+answer = input("What's the capital city of New Zealand?").lower()
 # Tell the user the answer
-if answer == "Wellington":
+if answer == "Wellington".lower():
     print("Correct!")
+    score += 5
 elif answer == "":
     print("Not sure?")
 else:
     print("Wrong!")
 print("The capital city of New Zealand is Wellington.")
+# End the quiz
+print("Well done {}. You finished the quiz. Your final score was {}".format(name, score))
