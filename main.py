a53b080b60e36fa3162ca862e4699ad091ac56ef
THESHADOWKNIFE <yaoy@wsc.school.nz>
2024-05-09 09:22:07 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # Get user's name
 name = input("What's your name?")
 # Greet the user and introduce the quiz
@@ -9,6 +10,11 @@ print("Welcome to the quiz,", name)
 print("This quiz is about capital cities of the world.")
 # Ask the user a question
 answer = input("What's the capital city of New Zealand?")
+# Tell the user the answer
 if answer == "Wellington":
     print("Correct!")
+elif answer == "":
+    print("Not sure?")
+else:
+    print("Wrong!")
 print("The capital city of New Zealand is Wellington.")
