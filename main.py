8d9807fb1331af5b919d68c3903f1dfa8dc48799
THESHADOWKNIFE <yaoy@wsc.school.nz>
2024-05-13 11:58:11 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+QUESTION_FORMAT = "{}\nA.{} B.{} C.{} D.{}"
 score = 0
 # Get user's name
 name = input("What's your name?")
@@ -9,11 +10,18 @@ print("Let's play a game.")
 print("Welcome to the quiz,", name)
 print("This quiz is about capital cities of the world.")
 # Ask the user a question
-answer = input("What's the capital city of New Zealand?").lower()
-# Tell the user the answer
-if answer == "Wellington".lower():
+question = "What's the capital city of New Zealand?"
+a = "Wellington"
+b = "Auckland"
+c = "London"
+d = "Christchurch"
+answer = input(QUESTION_FORMAT.format(question, a, b, c, d)).lower()
+# Tell the user the answerL
+if answer == a or answer == "a":
     print("Correct!")
     score += 5
+elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c":
+    print("That wasn't an option!")
 elif answer == "":
     print("Not sure?")
 else:
