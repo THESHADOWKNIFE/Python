c64c6a1a636f2ae44094baf2820de5112da241da
THESHADOWKNIFE <yaoy@wsc.school.nz>
2024-05-23 10:11:01 +1200

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,13 @@ ANSWERS = [1,2,3]
 
 play = "yes"
 
-# Get user's name
+# Ask the user their name
 name = input("What's your name?")
+
 # Greet the user and introduce the quiz
-if name == "Lorenzo":
-    print("That's cool.")
-    print("My name is Vegeta.")
-print("Let's play a game.")
 print("Welcome to the quiz,", name)
 print("This quiz is about capital cities of the world.")
+
 # Check number of question attempts
 while True:
     try:
@@ -31,33 +29,39 @@ while True:
         break
     except:
         print("That's not a number!")
+
 # Start the quiz
 while play == "yes":
+    score = 0
 
-    question_attempts = tries
-    while question_attempts > 0:
-        # Ask the user a question
-        question = "What's the capital city of New Zealand?"
-        a = "Wellington"
-        b = "Auckland"
-        c = "London"
-        d = "Christchurch"
-        answer = input("{}\nA.{} B.{} C.{} D.{}".format(question, a, b, c, d)).lower()
-        # Check the user the answer
-        if answer == a or answer == "a":
-            print("Correct!")
-            score += 5
-            break
-        elif answer != a and answer != "a" and answer != b and answer != "b" and answer != c and answer != "c" and answer != d and answer != "d":
-            print("That wasn't an option!")
-        elif answer == "":
-            print("Not sure?")
-        else:
-            print("Wrong!")
+    # Loop through each question/answers
+    for i in range(len(QUESTIONS)):
+        question_attempts = tries
+        while question_attempts > 0:
+            # Ask the user a question
+            answer = input(QUESTION_FORMAT.format(QUESTIONS[i], OPTIONS[i][0],
+                                                OPTIONS[i][1], OPTIONS[i][2], OPTIONS[i][3])).lower()
+            
+            # Check the user's answer
+            if answer == OPTIONS[i][ANSWERS[i]] or answer == SHORT_OPTIONS[ANSWERS[i]]:
+                print("Correct!")
+                score += 5
+                print(random.choice(GOOD_COMMENTS))
+                break
+            elif answer == "":
+                print("Not sure?")
+            elif answer in SHORT_OPTIONS or answer in OPTIONS[i]:
+                print("Wrong!")
+                print(random.choice(BAD_COMMENTS))
+            else:
+                print("That wasn't an option")
 
-        question_attempts -= 1
-    print("The capital city of New Zealand is Wellington.")
+
+            question_attempts -= 1
+        print("The answer is Wellington.")
+
     # End the quiz
     print("Well done {}. You finished the quiz. Your final score was {}".format(name, score))
+
     # Replay
     play = input("Do you want to play again?").lower()
